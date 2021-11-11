44f8bbab9a279baadc4481f4fc436a1af5caa195
Sveni2021 <sveni2021@gmail.com>
2021-11-11 17:54:32 +0100

    Start creating calculator

---
+++ calculator.js
@@ -0,0 +1,16 @@
+let containFC = document.createElement('div');
+containFC.style.height = '225px';
+containFC.style.width = '440px';
+containFC.style.backgroundColor = '#383838'
+cont.appendChild(containFC);
+let display = document.createElement('input');
+display.disabled;
+display.type = 'text';
+display.style.width = '300px';
+display.style.height = '50px';
+display.style.fontSize = '25px';
+display.style.backgroundColor = '#9e9e9e';
+containFC.appendChild(display);
+function createButton (val = 1){
+
+}
